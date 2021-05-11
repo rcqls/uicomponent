e26bbb63a33f1ba36f16a2a85ff1c831b3cd3b7a
R cqls <rdrouilh@gmail.com>
2021-05-11 13:14:05 +0200

    rename Component to ComponentChild

--- components.v
+++ components.v
@@ -4,10 +4,10 @@ import ui
 
 
 // All the components could be listed here to have an overall of all components
-pub fn component_doublelistbox(w ui.Component) &DoubleListBox {
+pub fn component_doublelistbox(w ui.ComponentChild) &DoubleListBox {
 	return &DoubleListBox(w.component)
 }
 
-pub fn component_toolbar(w ui.Component) &ToolBar {
+pub fn component_toolbar(w ui.ComponentChild) &ToolBar {
 	return &ToolBar(w.component)
 }
