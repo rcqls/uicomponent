491b0dcaa46f0e9513ebe85e493104940fbc7b68
R cqls <rdrouilh@gmail.com>
2021-05-15 02:43:42 +0200

    canvas_plus added

--- component_color.v
+++ component_color.v
@@ -4,20 +4,35 @@ import ui
 import gx
 import sokol.sgl
 
+const (
+	cb_sp      = 5
+	cb_hsv_col = 30
+	cb_nc      = 2
+	cb_nr      = 3
+)
+
+struct HSVColor {
+	h f64
+	s f64
+	v f64
+}
+
 [heap]
 struct ColorBox {
 mut:
-	simg C.sg_image
-	h    f64 = 0.5
-	s    f64 = 0.5
-	v    f64 = 0.5
-	rgb  gx.Color
+	simg    C.sg_image
+	h       f64 = 0.5
+	s       f64 = 0.5
+	v       f64 = 0.5
+	rgb     gx.Color
+	ind_sel int
+	hsv_sel []HSVColor = []HSVColor{len: uicomponent.cb_nc * uicomponent.cb_nr}
 pub mut:
 	layout     &ui.Stack // optional
 	cv_h       &ui.CanvasLayout
 	cv_sv      &ui.CanvasLayout
 	r_rgb_cur  &ui.Rectangle
-	cv_rgb_sel &ui.CanvasLayout
+	cv_hsv_sel &ui.CanvasLayout
 	// To become a component of a parent component
 	component voidptr
 }
@@ -27,60 +42,64 @@ pub struct ColorBoxConfig {
 }
 
 pub fn colorbox(c ColorBoxConfig) &ui.Stack {
-	mut cv_h := ui.canvas_layout({
+	mut cv_h := ui.canvas_plus(
 		width: 30
 		height: 256
 		on_draw: cv_h_draw
 		on_click: cv_h_click
 		on_mouse_move: cv_h_mouse_move
-	}, [])
-	mut cv_sv := ui.canvas_layout({
+	)
+	mut cv_sv := ui.canvas_plus(
 		width: 256
 		height: 256
 		on_draw: cv_sv_draw
 		on_mouse_move: cv_sv_mouse_move
 		on_click: cv_sv_click
-	}, [])
+	)
 	mut r_rgb_cur := ui.rectangle(
 		width: 30
 		height: 30
 		radius: 5
 	)
-	mut cv_rgb_sel := ui.canvas_layout({
+	mut cv_hsv_sel := ui.canvas_plus(
 		bg_radius: 5
 		bg_color: gx.rgb(220, 220, 220)
-	}, [ui.at(5, 5, ui.rectangle(
-		width: 20
-		height: 20
-		radius: 5
-	))])
+		on_draw: cv_sel_draw
+		on_click: cv_sel_click
+	)
 	mut layout := ui.row({
 		id: c.id
 		bg_color: gx.rgba(0, 0, 0, 200)
-		widths: [30., 256., 60]
-		heights: 256.
+		widths: [30., 256., ui.compact]
+		heights: [256., 256., ui.compact]
 		spacing: 10.
 		margin_: 10
 	}, [
 		cv_h,
 		cv_sv,
 		ui.column({
-			heights: [30., 45.]
-			widths: [30., 45]
+			heights: [30.,
+				(uicomponent.cb_hsv_col + uicomponent.cb_sp) * uicomponent.cb_nr +
+				uicomponent.cb_sp,
+			]
+			widths: [30.,
+				(uicomponent.cb_hsv_col + uicomponent.cb_sp) * uicomponent.cb_nc +
+				uicomponent.cb_sp,
+			]
 			spacing: 5.
-		}, [r_rgb_cur, cv_rgb_sel]),
+		}, [r_rgb_cur, cv_hsv_sel]),
 	])
 	mut cb := &ColorBox{
 		layout: layout
 		cv_h: cv_h
 		cv_sv: cv_sv
 		r_rgb_cur: r_rgb_cur
-		cv_rgb_sel: cv_rgb_sel
+		cv_hsv_sel: cv_hsv_sel
 	}
 	cb.cv_h.component = cb
 	cb.cv_sv.component = cb
 	cb.r_rgb_cur.component = cb
-	cb.cv_rgb_sel.component = cb
+	cb.cv_hsv_sel.component = cb
 	layout.component = cb
 	// init component
 	layout.component_init = colorbox_init
@@ -88,15 +107,16 @@ pub fn colorbox(c ColorBoxConfig) &ui.Stack {
 	return layout
 }
 
+// equivalent of init method for widget
+// automatically called in by the layout
 fn colorbox_init(layout &ui.Stack) {
 	mut cb := component_colorbox(layout)
 	cb.update_cur_color()
-	cb.update_sel_color()
+	for i in 0 .. (uicomponent.cb_nc * uicomponent.cb_nr) {
+		cb.hsv_sel[i] = HSVColor{f64(i) / (uicomponent.cb_nc * uicomponent.cb_nr), .75, .75}
+	}
 	cb.simg = create_dynamic_texture(256, 256)
 	cb.update_buffer()
-	println(ui.rgb_to_hsv(gx.rgb(0, 255, 0)))
-	println(ui.rgb_to_hsv(gx.rgb(255, 0, 0)))
-	println(ui.rgb_to_hsv(gx.rgb(0, 0, 255)))
 }
 
 fn cv_h_click(e ui.MouseEvent, c &ui.CanvasLayout) {
@@ -163,9 +183,40 @@ fn cv_sv_draw(mut c ui.CanvasLayout, app voidptr) {
 	sgl.end()
 	sgl.disable_texture()
 	c.draw_rounded_rect(int(cb.s * 256.) - 10, int((1. - cb.v) * 256.) - 10, 20, 20, 10,
-		ui.hsv_to_rgb(cb.h, 1., 1.))
+		ui.hsv_to_rgb(cb.h, 1 - cb.s, 1. - cb.v))
 	c.draw_rounded_rect(int(cb.s * 256.) - 7, int((1. - cb.v) * 256.) - 7, 14, 14, 7,
 		ui.hsv_to_rgb(cb.h, cb.s, cb.v))
+}
+
+fn cv_sel_click(e ui.MouseEvent, c &ui.CanvasLayout) {
+	mut cb := component_colorbox(c)
+	i := (e.x - uicomponent.cb_sp) / (uicomponent.cb_sp + uicomponent.cb_hsv_col)
+	j := (e.y - uicomponent.cb_sp) / (uicomponent.cb_sp + uicomponent.cb_hsv_col)
+	cb.ind_sel = i + j * uicomponent.cb_nc
+	// println("($i, $j) -> ${cb.ind_sel}")
+	hsv := cb.hsv_sel[cb.ind_sel]
+	cb.h, cb.s, cb.v = hsv.h, hsv.s, hsv.v
+	cb.update_buffer()
+}
+
+fn cv_sel_draw(mut c ui.CanvasLayout, app voidptr) {
+	cb := component_colorbox(c)
+	mut hsv := HSVColor{}
+	mut h, mut s, mut v := 0., 0., 0.
+	ii, jj := cb.ind_sel % uicomponent.cb_nc, cb.ind_sel / uicomponent.cb_nc
+	c.draw_rounded_rect(uicomponent.cb_sp + ii * (uicomponent.cb_hsv_col + uicomponent.cb_sp) - 1,
+		uicomponent.cb_sp + jj * (uicomponent.cb_hsv_col + uicomponent.cb_sp) - 1,
+		uicomponent.cb_hsv_col + 2, uicomponent.cb_hsv_col + 2, .25, gx.black)
+	for j in 0 .. uicomponent.cb_nr {
+		for i in 0 .. uicomponent.cb_nc {
+			hsv = cb.hsv_sel[i + j * uicomponent.cb_nc]
+			h, s, v = hsv.h, hsv.s, hsv.v
+			c.draw_rounded_rect(uicomponent.cb_sp + i * (uicomponent.cb_hsv_col + uicomponent.cb_sp),
+				uicomponent.cb_sp + j * (uicomponent.cb_hsv_col + uicomponent.cb_sp),
+				uicomponent.cb_hsv_col, uicomponent.cb_hsv_col, .25, ui.hsv_to_rgb(h,
+				s, v))
+		}
+	}
 }
 
 fn (mut cb ColorBox) update_cur_color() {
@@ -173,10 +224,8 @@ fn (mut cb ColorBox) update_cur_color() {
 }
 
 fn (mut cb ColorBox) update_sel_color() {
-	mut r_sel := cb.cv_rgb_sel.children[0]
-	if mut r_sel is ui.Rectangle {
-		r_sel.color = ui.hsv_to_rgb(cb.h, cb.s, cb.v)
-	}
+	// cb.r_sel.color = ui.hsv_to_rgb(cb.h, cb.s, cb.v)
+	cb.hsv_sel[cb.ind_sel] = HSVColor{cb.h, cb.s, cb.v}
 }
 
 pub fn (mut cb ColorBox) update_buffer() {
